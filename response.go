cf2bbd8afa7377a9462a83168666f3a52f1618b3
Tomasz Sadowy <tomaszsadowypriv@gmail.com>
2024-08-10 15:26:49 +0100

    readLine func added

--- response.go
+++ response.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"io"
 )
 
 const (
@@ -22,4 +23,23 @@ type Value struct {
 
 type Response struct {
 	reader *bufio.Reader
+}
+
+func NewResponse(rd io.Reader) *Response {
+	return &Response{reader: bufio.NewReader(rd)}
+}
+
+func (r *Response) readLine() (line []byte, n int, err error) {
+	for {
+		_byte, err := r.reader.ReadByte()
+		if err != nil {
+			return nil, 0, err
+		}
+		n += 1
+		line = append(line, _byte)
+		if len(line) >= 2 && line[len(line)-2] == '\r' {
+			break
+		}
+	}
+	return line[:len(line)-2], n, nil
 }
