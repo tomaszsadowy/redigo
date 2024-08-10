482844764b934feb11890371c30bfad1183d388b
Tomasz Sadowy <tomaszsadowypriv@gmail.com>
2024-08-10 15:24:09 +0100

    create server

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
 func main() {
-	fmt.Println("Tomka nowy projekt")
+	fmt.Println("Listening using port :6379")
+
+	// creating the server
+	listen, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
